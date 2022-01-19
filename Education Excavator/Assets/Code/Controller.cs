a353ec4ffa449c9f5b113de74a4e59d9ffef3c54
TeriqueC <teriquecarnegie@gmail.com>
2022-01-19 21:35:48 +0000

    working sql

--- Education Excavator/Assets/Code/Controller.cs
+++ Education Excavator/Assets/Code/Controller.cs
@@ -13,6 +13,7 @@ public class Controller : MonoBehaviour
     void Start()
     {
         player.GetComponent<Player>();
+        //generator.createTable();
         setQuestion();
     }
 
--- Education Excavator/Assets/Code/QuestionGenerator.cs
+++ Education Excavator/Assets/Code/QuestionGenerator.cs
@@ -6,17 +6,37 @@ using System;
 
 public class QuestionGenerator
 {
-    string dbName= "URI=file: Education database.db";
+    string dbName = "URI= file: Test database";
+    string sql;
+    
+    public void createTable(){
+         SqliteConnection connection = new SqliteConnection(dbName);
+        connection.Open();
+        SqliteCommand Command = connection.CreateCommand();
+
+        sql = "CREATE TABLE IF NOT EXISTS Test (test_id INT)";
+
+        Command.CommandText = sql;
+        Command.ExecuteNonQuery();
+
+        sql= "INSERT INTO Test (test_id) values ('1')";
+
+        Command.CommandText= sql;
+        Command.ExecuteNonQuery();
+    }
+    
     public string getQuestion(){
         SqliteConnection connection = new SqliteConnection(dbName);
         connection.Open();
         SqliteCommand Command = connection.CreateCommand();
 
-        Command.CommandText = "SELECT question FROM Questions WHERE question_id = 1";
+        sql = "SELECT * FROM Question";
+
+        Command.CommandText = sql;
         SqliteDataReader reader = Command.ExecuteReader();
         reader.Read();
 
-        string question = reader.GetString(1); 
+        string question = reader.GetString(0); 
 
         connection.Close();
         return question;
