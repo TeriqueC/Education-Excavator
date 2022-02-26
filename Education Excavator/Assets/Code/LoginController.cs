02c532b317e25375039d21030a8bc09fed636036
TeriqueC <teriquecarnegie@gmail.com>
2022-02-26 16:01:01 +0000

    cleaning up

--- Education Excavator/Assets/Code/LoginController.cs
+++ Education Excavator/Assets/Code/LoginController.cs
@@ -28,13 +28,8 @@ namespace EducationExcavator{
         public void loginEnter(){
             username = loginUserName.text;
             password = loginPassword.text;
-            //string word = loginPassword.text;
             Debug.Log(password);
-            //Debug.Log(word);
             int passcode= hashing.hash(password);
-            //int wordcode= hashing.hash(word);
-            Debug.Log("password attempt"+passcode);
-            //Debug.Log("word"+ wordcode);
             bool conformation = hashing.CheckDetails(username, passcode);
             if(conformation == true){
                 playerId = hashing.retrieveId(username);
