b049b1eb722f4e6fcf7b9a9f6ccd705c1628ce6b
TeriqueC <teriquecarnegie@gmail.com>
2022-03-07 21:21:00 +0000

    minor game adjustments

--- Education Excavator/Assets/Code/Asteroid.cs
+++ Education Excavator/Assets/Code/Asteroid.cs
@@ -6,7 +6,7 @@ namespace EducationExcavator
 {
     public class Asteroid : MonoBehaviour
     {
-        private float asteriod_speed= 0.45f;
+        private float asteriod_speed= 0.3f;
         private float asteriod_xpos;
         private float asteroid_ypos = 2f;
 
